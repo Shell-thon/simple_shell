2f033a299bbf29fedd5ca722351e2793058c704c
ifezue501 <flasysexy501@yahoo.com>
2022-12-10 19:01:33 -0800

    added _wordNumber.c file

---
+++ _wordNumber.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * _wordNumber - returns word number
+ *
+ * @prmString: char pointer
+ *
+ * Return: Word number
+ */
+int _wordNumber(char *prmString, char *prmSeparators)
+{
+	int cLoop = 0, count = 0;
+
+	while (prmString[cLoop] != '\0')
+	{
+		if (
+			(
+			 	_inArray(prmString[cLoop], prmSeparators) != 1 ||
+				prmString[cLoop] != '\0'
+			 ) &&
+			(
+			 	_inArray(prmString[cLoop + 1], prmSeparators) == 1 ||
+				prmString[cLoop + 1] == '\0'
+			)
+		)
+			count++;
+		cLoop++;
+	}
+
+	return (count);
+}
